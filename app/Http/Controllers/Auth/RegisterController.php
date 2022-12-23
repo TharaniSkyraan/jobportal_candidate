f83d5efa1ebf7bfb7d4ce3fb97ae76584e9052d0
TharaniSkyraan <tharani@skyraan.com>
2022-12-23 00:26:05 -0500

    update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,7 @@ class RegisterController extends Controller
      */
     public function __construct(Request $request)
     {
-        $this->middleware('auth',function ($request, $next) {
-          dd('test');
-        })->except('getVerification', 'getVerificationError');
+        $this->middleware('check_auth')->except('getVerification', 'getVerificationError');
     }
 
     /**
