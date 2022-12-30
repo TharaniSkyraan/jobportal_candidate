2ab5e183d0708e25fbe417671dc4264534e2f227
TharaniSkyraan <tharani@skyraan.com>
2022-12-30 22:34:45 +0530

    update

--- resources/views/user/signup/career_info.blade.php
+++ resources/views/user/signup/career_info.blade.php
@@ -12,6 +12,7 @@
 
 @php
 dd($ip_data);
+
     $country_id = (!empty($user->country_id))?$user->country_id:$ip_data->country_id;
     $country = (!empty($user->country_id))?$user->getCountry('country'):$ip_data->geoplugin_countryName;
 @endphp
