cf10876c17269954a3b50a7089cdbdc582048c35
TharaniSkyraan <tharani@skyraan.com>
2022-12-31 10:56:29 +0530

    update mail

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -282,10 +282,10 @@ class LoginController extends Controller
         $user->save();
             
         $user = User::findOrFail(Session::get('id'));
-        // Auth::login($user, true); 
-        // UserVerification::generate($user);
-        // UserVerification::send($user, 'User Verification', config('mail.recieve_to.address'), config('mail.recieve_to.name'));
-        // Auth::logout();
+        Auth::login($user, true); 
+        UserVerification::generate($user);
+        UserVerification::send($user, 'User Verification', config('mail.recieve_to.address'), config('mail.recieve_to.name'));
+        Auth::logout();
 
         return view('auth.verify_otp',compact('user'));      
 
