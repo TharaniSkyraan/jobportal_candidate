a861612bb9d9d1cddc98d39394403e0133bda8a0
TharaniSkyraan <tharani@skyraan.com>
2023-01-19 18:04:20 +0530

    update

--- resources/views/user/dashboard/job_detail.blade.php
+++ resources/views/user/dashboard/job_detail.blade.php
@@ -78,9 +78,198 @@ ul{
     color: #666666;
     font-size: 13px !important;
 }
-        </style>
-	@include('layouts.header')
-	@include('layouts.side_navbar')
+
+    #received_cnt{
+        margin: 0 auto;
+        /* box-shadow: 0 3px 10px rgb(0 0 0 / 10%); */
+    }
+    #received_cnt .main_assementcrd{
+        margin-bottom: 0px;
+    }
+    #cdate_assesment li{
+        margin-bottom: 15px;
+    }   
+    #cdate_assesment .cont_act{
+        box-shadow: 0 3px 10px rgb(0 0 0 / 10%);
+        padding: 15px 0px 0px 15px !important;
+        margin-top: 20px;
+        border-radius: 10px;
+
+    }
+    #cdate_assesment .profile_card{
+        background-color: #F3F7FE !important;
+
+    }
+
+    #cdate_assesment .profile_card .ple_img{
+        width: 45%;
+        border-radius: 50%;
+        padding: 20px;
+        line-height: 1.3rem;
+        height: 150px;
+    }
+    #cdate_assesment .profile_card{
+        margin: 0 auto;
+        border: 1px solid rgb(0 0 0 / 6%);
+        border-radius: 10px;
+        box-shadow: none;
+        background-color: #eafbf1 !important;
+    }
+
+    #cdate_assesment::-webkit-scrollbar {
+        display: none;
+    }
+
+    #cdate_assesment{
+        overflow-y: overlay;
+        height: 500px;
+    }
+
+    #cdate_assesment .profile_card .btnact_st{
+        box-shadow: 0px 0px 3px 2px #d7d5d5;
+        background-color: #fcfeff;
+        padding: 0.5rem;
+        background-color: #fff;
+        border-radius: 0.25rem;
+        max-width: 100%;
+    }
+
+    #cdate_assesment hr{
+        height: 2px;
+        background-color: #C4C4C4;
+    }
+    #cdate_assesment .fa-book{
+        font-size: 30px;
+    }
+    #cdate_assesment .message_btn{
+        width: fit-content;
+        margin: 0 auto;
+        background-color: #4285F4;
+        color: #fff;
+        border-radius: 20px;
+        border: 1px solid #4285F4;
+        padding: 5px 20px 5px 20px;
+    }
+    #cdate_assesment thead{
+        background-color: #eafbf1 !important;
+    }
+    #cdate_assesment tbody{
+        background-color: #fff;
+    }
+    #cdate_assesment .fa-phone, #cdate_assesment .fa-envelope, #cdate_assesment .fa-linkedin{
+        align-items: center;
+        width: 30px;
+        background-color: #1572e8b5;
+        height: 30px;
+        justify-content: center;
+        display: inline-grid;
+        border-radius: 50%;
+        border: 2px solid;
+        font-size: 12px;
+        color:#fff !important;
+
+    }
+   
+    #cdate_assesment {
+        height:300px;
+        top: 0;
+        bottom: 0;
+        position: inherit;        
+    }
+
+    #cdate_assesment .table td{
+        border-color: transparent !important;
+    }
+    #assessment .nav-tabs {
+        border-bottom: 1px solid #efeffe;
+        background-color: #EFF4FE;
+        padding: 10px;
+        border-bottom: none;
+    }
+    #assessment #applied-tab{
+        margin-right: 120px;
+    }
+    #assessment .nav-link.active{
+        border-bottom: 2px solid #ec584c !important;
+        background-color: transparent;
+        border-color:transparent;
+        font-weight: bolder;
+    }
+
+    #assessment .nav-link{
+        color: #000;
+        border:transparent;
+    }
+    #cdate_assesment .assessment_card{
+        border: 2px solid #4285F4;
+        padding: 20px;
+        border-radius: 5px;
+        background-color: #fff !important;
+    }
+    #cdate_assesment .fa-info{
+        background-color: #4285f4;
+        width: 18px;
+        align-items: center;
+        justify-content: center;
+        display: inline-flex;
+        color: #fff;
+        border-radius: 50%;
+        height: 18px;
+        font-size: 12px;
+    }
+
+    #cdate_assesment .fa-check, #cdate_assesment .fa-close{
+        width: 25px;
+        align-items: center;
+        justify-content: center;
+        display: inline-flex;
+        color: #fff;
+        border-radius: 50%;
+        height: 25px;
+        font-size: 12px;
+    }
+
+    #cdate_assesment .fa-check{
+        background: #6CD038;
+    }
+
+    #cdate_assesment .fa-close{
+        background: #FF5B5B;
+    }
+
+    .assessment_modal{
+        max-width: 50%;
+    }.assessment_modal .modal-content{
+        padding: 10px;
+        width: fit-content;
+        background: #fff;
+        height: auto;
+    }.assessment_modal img{
+        width: 100%;
+    }.assessment_modal p{
+        color: #808080;
+        font-weight: bolder;
+        font-size: 16px;
+    }#screeningQuiz72ers3 .modal-header{
+        background-color: #d5e4fd;
+    }#screeningQuiz72ers3 .modal-header h3{
+        font-weight: bolder;
+    }#screeningQuiz72ers3 .modal-content{
+        padding: 0px;
+    }#screeningQuiz72ers3 .close{
+        border: 0px;
+        background: #fff;
+        border-radius: 50%;
+        width: 28px;
+    }.assessment_modal .card{
+        box-shadow: none;
+    }
+    .assessment_modal .next-btn{
+        padding: 9px 20px;
+    }
+</style>
+@include('layouts.header')
+@include('layouts.side_navbar')
 
 <div class="main-panel main-panel-custom">
     <div class="content">
@@ -106,6 +295,20 @@ ul{
                                         <label class="japplied-btn" id="japplied-btn" ><img class="imagesz-2" src="{{url('site_assets_1/assets/img/Shortlist.png')}}" alt="Shortlist"> <span class="fw-bolder fs-6">Shortlisted</span></label>
                                         @elseif($application_status=='reject')
                                         <label class="japplied-btn" id="japplied-btn" ><img class="imagesz-2" src="{{url('site_assets_1/assets/img/Rejected.png')}}" alt="Rejected"> <span class="fw-bolder fs-6">Rejected</span></label>
+                                        @else
+                                            @if(count($job->screeningquiz)!=0)
+                                                <button class="btn btn-lg p-1 shadow-sm bg-color-blue rounded-pill japply-btn " id="japplybtn" data-bs-toggle="modal" href="#screeningQuiz72ers3" role="button">
+                                                    <img class="image-size" src="{{url('site_assets_1/assets/img/apply2.png')}}" alt="apply"> <span class="fw-bold">Apply</span>
+                                                </button>
+                                            @else
+                                                <button class="btn btn-lg p-1 shadow-sm bg-color-blue rounded-pill japply-btn japplybtn" id="japplybtn">
+                                                    <img class="image-size" src="{{url('site_assets_1/assets/img/apply2.png')}}" alt="apply"> <span class="fw-bold">Apply</span>
+                                                </button>
+                                            @endif
+                                            
+                                            <label class="japplied-btn">
+                                                <img class="imagesz-2" src="{{url('site_assets_1/assets/img/Shortlist.png')}}" alt="Applied"> <span class="fw-bolder fs-6">Applied</span>
+                                            </label>
                                         @endif
                                         {{-- <div class="mx-3">
                                             <img class="image-size" src="{{url('site_assets_1/assets/img/star_unfilled.png')}}" alt="bookmark">
@@ -362,6 +565,119 @@ ul{
 
     </div>
 </div>
+
+    <div class="modal fade" id="screeningQuiz72ers3" tabindex="-1" role="dialog" aria-labelledby="screeningQuiz72ers3Label2" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered assessment_modal">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="exampleModalLongTitle">Screening Questions</h3>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form class="form" id="screeningQuiz" action="{{route('job.apply', $job->slug)}}" method="post">
+                    @csrf
+                    {!! Form::hidden('is_login',null, array('id'=>'is_login')) !!}
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-6 col-md-6 align-self-center mobile_m">
+                                <img src="{{asset('images/Screening.png')}}" alt="">
+                            </div>
+                            <div class="col-12 col-md-12 col-lg-6 align-self-center">
+                                <section id="cdate_assesment">
+                                    <div class="card ass_details p-2">
+                                        <div class="row">
+                                            {{-- Question --}}
+                                            @foreach ($job->screeningquiz as $key => $quiz)
+                                            <div class="col-md-12 quiz" data-bp="{{$quiz->breakpoint}}" data-dsw3w14="{{$quiz->quiz_code}}" data-dsw3w15="{{$quiz->answer_type}}">
+                                                <p class="h3 mt-4 mb-2">{{$quiz->candidate_question}}</p>
+                                                
+                                                @if($quiz->answer_type=='text')
+                                                    {{ Form::text('answer_'.$quiz->quiz_code, null, array('class'=>'form-control e1ex0nj0', 'id'=>'answer_'.$quiz->quiz_code, 'placeholder'=>__(' '))) }}
+                                                @elseif($quiz->answer_type=='single')
+                                                    @foreach (json_decode($quiz->candidate_options) as $key1 => $option)                           
+                                                        <div class="form-check">
+                                                            <input class="form-check-input e1ex0nj0" type="radio" name="answer_{{$quiz->quiz_code}}" value="{{ $option }}" id="answerradio{{$quiz->quiz_code}}_{{ $key1 }}" @if($key1==0) checked @endif>
+                                                            <label class="form-check-label" for="answerradio{{$quiz->quiz_code}}_{{ $key1 }}">
+                                                                {{ $option }}
+                                                            </label>
+                                                        </div>                                                
+                                                    @endforeach
+                                                @elseif($quiz->answer_type=='multiple')
+                                                    @foreach (json_decode($quiz->candidate_options) as $key1 => $option)                           
+                                                        <div class="form-check">
+                                                            <input class="form-check-input e1ex0nj0" type="checkbox" name="answer_{{$quiz->quiz_code}}[]" value="{{ $option }}" id="answercheckbox{{$quiz->quiz_code}}_{{ $key1 }}">
+                                                            <label class="form-check-label" for="answercheckbox{{$quiz->quiz_code}}_{{ $key1 }}">
+                                                                {{ $option }}
+                                                            </label>
+                                                        </div>
+                                                    @endforeach
+                                                @elseif($quiz->answer_type=='textarea')
+                                                    {{ Form::textarea('answer_'.$quiz->quiz_code, null, array('class'=>'form-control e1ex0nj0', 'id'=>'answer_'.$quiz->quiz_code, 'placeholder'=>__(' '))) }}
+                                                @elseif($quiz->answer_type=='select')
+                                                @php
+                                                    $options = json_decode($quiz->candidate_options);
+                                                    $options = array_combine($options, $options);
+                                                @endphp
+                                                    {{ Form::select('answer_'.$quiz->quiz_code, ['' => __('Select')]+$options??'', null, array('class'=>'form-select', 'id'=>'answer_'.$quiz->quiz_code)) }}
+                                                @endif
+                                                <span class="es2wa7s text-danger"> </span>
+
+                                            </div>
+                                            @endforeach
+                                            <span class="es2wa7sd text-danger"> </span>
+
+                                            {{-- // Question --}}
+                                        </div>
+                                    </div>                  
+                                </section>    
+                            </div>
+                        </div>
+                    </div>
+                  
+                    <div class="container">
+                        <div class="d-grid gap-2 mx-3 mb-4 d-md-flex justify-content-md-between">
+                            @if($breakpoint==null)
+                                <a class="btn previous-btn skip-submit"> Skip and apply</a>
+                            @endif
+                            <a class="btn previous-btn" data-bs-dismiss="modal"> Skip</a>
+                            <button type="button" class="btn next-btn submit">Submit and Apply</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=6315c4c30b5e930012a9c49e&product=inline-share-buttons' async='async'></script>
+@php
+    $applied = Auth::user()?(Auth::user()->isAppliedOnJob($job->id)??false):false;
+@endphp
+<script>
+    var applied = '{{ $applied }}';
+    var baseurl = '{{ url("/") }}/';
+    var is_login = 1;
+    var save_req_url = "{{ route('job.save', $job->slug) }}";
+    var apply_req_url = "{{ route('job.apply', $job->slug) }}" ;
+
+    //company header
+    $('#myHeader').hide();
+
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+    $('#myHeader').show();
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+    $('#myHeader').hide();
+        header.classList.remove("sticky");
+    }
+    } 
+   
+</script>
+<script type="text/javascript" src="{{ asset('site_assets_1/assets/2e9ejr3/js/destail.e2k3eu0.js') }}"></script>
 
 @endsection
