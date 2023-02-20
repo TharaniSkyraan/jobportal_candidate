665dab3585a3a66ca81897871b13db6668733fbe
TharaniSkyraan <tharani@skyraan.com>
2023-02-20 12:44:28 +0530

    update mete tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />        
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
     @include('layouts.meta')
     <!-- Favicons -->
     <link href="{{ asset('site_assets_1/favicon.ico')}}" rel="icon">
