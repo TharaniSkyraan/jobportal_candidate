538c2a730888338056624bcd15c0ca8636529da5
TharaniSkyraan <tharani@skyraan.com>
2023-02-20 10:47:39 +0530

    update

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,10 +1,10 @@
 @if(isset($seo))
+    <title>{{ $seo->seo_title ?? ''}}</title>
     <meta name="title" content="{{ $seo->seo_title ?? ''}}">
     <meta name="description" content="{{ $seo->seo_description ?? ''}}"/>
     <link rel="canonical" href="{{ Request::url() ?? 'https://www.mugaam.com/'}}"/>
     @if(!empty($seo->seo_keywords))
         <meta name="keywords" content="{{ $seo->seo_keywords }}">
-        <title>{{ $seo->seo_title ?? ''}}</title>
     @endif      
 @endif
 <!-- Facebook Meta Tags -->
