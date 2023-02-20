0e56c39654ebe896c1af67c14cfb9f36e1953856
TharaniSkyraan <tharani@skyraan.com>
2023-02-20 03:09:36 -0500

    u[date

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -3,22 +3,8 @@
     <meta name="description" content="{{ $seo->seo_description ?? ''}}"/>
     <link rel="canonical" href="{{ Request::url() ?? 'https://www.mugaam.com/'}}"/>
     @if(!empty($seo->seo_keywords))
-        <meta name="keywords" content="{{ $seo->seo_keywords }}">
-        <title>{{ $seo->seo_title ?? ''}}</title>
+    <meta name="keywords" content="{{ $seo->seo_keywords }}">
+    <title>{{ $seo->seo_title ?? ''}}</title>
     @endif      
 @endif
-<!-- Facebook Meta Tags -->
-<!-- <meta property="og:url" content="">
-<meta property="og:type" content="">
-<meta property="og:title" content="">
-<meta property="og:description" content="">
-<meta property="og:image" content="">
-<meta property="og:locale" content="en_US"/> -->
-
-<!-- Twitter Meta Tags -->
-<!-- <meta name="twitter:card" content="">
-<meta property="twitter:domain" content="">
-<meta property="twitter:url" content="">
-<meta name="twitter:title" content="">
-<meta name="twitter:description" content="">
-<meta name="twitter:image" content=""> -->
+<meta name="google-site-verification" content="BzvgVIU65gOXHATWh24LSGse9TnNKNm57QaGkhrmpQs"/>
