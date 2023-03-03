4f2eae5c2fa29f1f685ad9444a994e590785958e
TharaniSkyraan <tharani@skyraan.com>
2023-03-03 05:38:54 -0500

    update

--- resources/views/sitemap/job_title_location.blade.php
+++ resources/views/sitemap/job_title_location.blade.php
@@ -1,7 +1,11 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-@foreach ($titles as $title)  
-    <sitemap>
-        <loc>{{ route('sitemapjobtitlelocation',$title->designation) }}</loc>
-        <lastmod>{{ $title->created_at->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>  
-@endforeach
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    @foreach ($titles as $title)  
+      <url>
+          <loc>{{ route('sitemapjobtitlelocation',$title->designation) }}</loc>
+            <lastmod>{{ $title->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>0.8</priority>
+        </url>  
+    @endforeach
+</urlset>
