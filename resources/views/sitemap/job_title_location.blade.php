9e84d1b59877ea699b95fa14ea746b923e72b557
TharaniSkyraan <tharani@skyraan.com>
2023-03-03 14:25:09 +0530

    sitemap

--- resources/views/sitemap/job_title_location.blade.php
+++ resources/views/sitemap/job_title_location.blade.php
@@ -1,4 +1,5 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($titles as $title)  
     <sitemap>
         <loc>{{ route('sitemapjobtitlelocation',$title->designation) }}</loc>
@@ -7,3 +8,4 @@
         <priority>0.8</priority>
     </sitemap>  
 @endforeach
+</sitemapindex>
