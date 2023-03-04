a7ce86cac2e9031cda0bcf4f5839fe75141bdd4f
TharaniSkyraan <tharani@skyraan.com>
2023-03-04 12:45:26 +0530

    update

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -17,24 +17,8 @@
         <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod>
     </sitemap>
     <sitemap>
-        <loc>{{url('/sitemapjobtitlelocation.xml')}}</loc>
-        <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
         <loc>{{url('/sitemapjobtype.xml')}}</loc>
         <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>{{url('/sitemapjobtypelocation.xml')}}</loc>
-        <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>{{url('/sitemapjobtypetitle.xml')}}</loc>
-        <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>{{url('/sitemapjobtypetitlelocation.xml')}}</loc>
-        <lastmod>{{ Carbon\Carbon::now()->tz('UTC')->toAtomString() }}</lastmod> 
     </sitemap>
 </sitemapindex>
 
