a499bdeaea107f5b350024a42b14fc61ed0a9563
TharaniSkyraan <tharani@skyraan.com>
2023-03-04 14:50:08 +0530

    u[date

--- resources/views/sitemap/job_type_title_location.blade.php
+++ resources/views/sitemap/job_type_title_location.blade.php
@@ -1,7 +1,7 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($titles as $title) 
-        @foreach ($types as $type)  
+        @foreach($title->jobtypes as $type)  
         <sitemap> 
             <loc>{{ route('sitemapjobtypetitlelocation', [$title->designation,$type->id]) }}</loc>
             <lastmod>{{ $type->created_at->tz('UTC')->toAtomString() }}</lastmod>
