6445ed487bf8ba434be86c87789d041ceb5c86ce
TharaniSkyraan <Tharani@skyraan.com>
2023-05-29 12:22:37 +0530

    update

--- routes/api_routes/job.php
+++ routes/api_routes/job.php
@@ -4,14 +4,14 @@
     use App\Http\Controllers\Api\Job\MyJobsController;
 
     Route::post('search-job', [JobsController::class, 'searchJob']);
-    Route::get('jobdetail/{slug}', [JobsController::class, 'jobDetail']);
+    Route::get('job-detail/{slug}', [JobsController::class, 'jobDetail']);
     Route::get('company-detail/{slug}', [JobsController::class, 'companyDetail']);
 
-    Route::post('apply-job/{slug}', [MyJobsController::class, 'ApplyJob']);
-    Route::post('save-job/{slug}', [MyJobsController::class, 'Savejob']);
-    Route::get('applied-job-list', [MyJobsController::class, 'appliedJobsList']);
-    Route::get('saved-job-list', [MyJobsController::class, 'savedJobsList']);
-
-    
+    Route::middleware('auth:api')->group( function () {
+        Route::post('apply-job/{slug}', [MyJobsController::class, 'ApplyJob']);
+        Route::post('save-unsave-job/{slug}', [MyJobsController::class, 'Savejob']);
+        Route::get('applied-job-list', [MyJobsController::class, 'appliedJobsList']);
+        Route::get('saved-job-list', [MyJobsController::class, 'savedJobsList']);
+    });
 
 ?>
