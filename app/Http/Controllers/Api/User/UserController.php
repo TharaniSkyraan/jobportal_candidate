79343545fd0d8d6cf4e2e01b249ddffb6f945e7b
TharaniSkyraan <Tharani@skyraan.com>
2023-10-31 10:31:18 +0530

    update

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -48,6 +48,7 @@ class UserController extends BaseController
                 'resume_url' => $user->getDefaultCv()->cv_file??"",
                 'resume_id' => $user->getDefaultCv()->id??0,
                 'user_token' => $user->token,
+                'updated_at' => $user->updated_at,
                 'profile_summary' => url('api/profile_summary/'.$user->id),
             ),
             'genders' => DataArrayHelper::langGendersApiArray(),    
