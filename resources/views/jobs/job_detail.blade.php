1bbe3bef42135dbe3b3872008d61bc497deb385b
JayavelSkyraan <jayavel@skyraan.com>
2023-11-01 13:27:58 +0530

    changes

--- resources/views/jobs/job_detail.blade.php
+++ resources/views/jobs/job_detail.blade.php
@@ -109,12 +109,12 @@
                     
                     <div class="col-md-12 col-12">
                         <div class="row">
-                            <div class="col-md-4 col-5 col-xl-4 col-lg-4 align-self-center">
+                            <div class="col-md-4 col-5 col-xl-5 col-lg-5 align-self-center">
                                 <div class="postedrs_dt mgmw mb-0">
                                     <img src="{{asset('images/detailpage/posted_icon.svg')}}" alt="posted-date" class="icon_rs" draggable="false"><span class="mblef">Posted</span> {{ MiscHelper::timeSince($job->posted_date) }}
                                 </div>
                             </div>
-                            <div class="col-md-6 col-5 col-xl-6 col-lg-5 align-self-center">
+                            <div class="col-md-6 col-5 col-xl-5 col-lg-5 align-self-center">
                                 <div class="immediate_ic mgmw mb-0">
                                     <img src="{{asset('images/detailpage/immediate.svg')}}" alt="immediate-join" class="icon_rs" draggable="false">Immediate <span class="mblef">Join</span>
                                 </div>
