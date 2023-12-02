4c7dc926aa085dcae8f8fc874680aa2fe342e263
TharaniSkyraan <Tharani@skyraan.com>
2023-12-02 16:40:49 +0530

    update

--- app/Http/Requests/Api/User/UpdateProfileRequest.php
+++ app/Http/Requests/Api/User/UpdateProfileRequest.php
@@ -32,7 +32,7 @@ class UpdateProfileRequest extends Request
         $email = Auth::user()->email;
         $rules =  [
             'name' => 'required|max:80',
-            'phone' => 'required|phone|unique:users,null,id,deleted_at,NULL,email,'.$email,            
+            'phone' => 'required|unique:users,null,id,deleted_at,NULL,email,'.$email,            
             'image' => 'required',
             // 'alternative_phone' => 'required',
             'gender' => 'required',
