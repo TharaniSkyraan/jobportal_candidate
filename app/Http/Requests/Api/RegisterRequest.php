1cbc339911cb5246d7539dc5fc49def7c1f9338d
TharaniSkyraan <Tharani@skyraan.com>
2023-12-02 16:56:45 +0530

    update

--- app/Http/Requests/Api/RegisterRequest.php
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -35,7 +35,7 @@ class RegisterRequest extends Request
         ];
         if($this->provider==null)
         {            
-            $rules['phone'] = 'required|unique:users,phone,0,id,deleted_at,NULL,email,'.$this->email;
+            $rules['phone'] = 'required|unique:users,phone,'.($this->email??null).',email,deleted_at,NULL';
             $rules['password'] = 'required|max:30';
         }
 
