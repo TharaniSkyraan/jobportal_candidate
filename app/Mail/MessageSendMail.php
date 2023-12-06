4fd163c1b181381e8ab27c9a8fff9be60953ff70
TharaniSkyraan <Tharani@skyraan.com>
2023-12-06 12:45:56 +0530

    update

--- app/Mail/MessageSendMail.php
+++ app/Mail/MessageSendMail.php
@@ -31,13 +31,13 @@ class MessageSendMail extends Mailable
     public function build()
     {
         $data = $this->data;
-        \Log::info($data);
-        return $this->from($data['company_email'], $data['company_name'])
-                        ->replyTo($data['company_email'], $data['company_name'])
-                        ->to(config('mail.support_recieve_to.address'), config('mail.support_recieve_to.name'))
-                        ->subject('New message received from '.$data['user_name'].' for '.$data['job_title'])
-                        ->markdown('emails.send_message')
-                        ->with(['data'=>$data]);
+        \Log::info($data['company_email']);
+        return $this->from($data['user_email'], $data['user_name'])
+                    ->to($data['company_email'], $data['company_name'])                        
+                    ->subject('New message received from '.$data['user_name'].' for '.$data['job_title'])
+                    ->markdown('emails.send_message')
+                    ->with(['data'=>$data]);
+                        
     }
 
 }
