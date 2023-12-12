c459b0a4c4adcdf7b4507377b15a270310b87fb4
TharaniSkyraan <Tharani@skyraan.com>
2023-12-12 20:10:52 +0530

    update

--- app/Http/Controllers/Api/Job/JobsController.php
+++ app/Http/Controllers/Api/Job/JobsController.php
@@ -360,7 +360,7 @@ class JobsController extends BaseController
             'about_company' => $job->company->description??'',
             'redirect_url' => (!empty($job->reference_url))?$job->reference_url:'',
             'job_status' => (!empty($job->deleted_at) || $job->is_active==3)?'Job is expired or closed.':(($job->is_active==2)?'Job is In-active':''), 
-            'website_link' => url('/detail/').$job->slug
+            'website_link' => url('/detail').'/'.$job->slug
         );
 
         $jobs = $this->fetchJobs($job->title, '', [], 10);
@@ -444,6 +444,7 @@ class JobsController extends BaseController
         $company->no_of_employees = $company->no_of_employees??'';
         $company->company_image = $company->company_image??"";
         $company->no_of_offices = $company->no_of_offices??'';
+        $company->description = $company->description??'';
         $company->industry = DataArrayHelper::industryParticular($company->industry_id??0);
 
         $companyjobs = array_map(function ($companyjob) use($user) {
