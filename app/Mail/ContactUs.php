fe21d23579d9e612fe65c2b673f105cb2942079d
TharaniSkyraan <Tharani@skyraan.com>
2023-12-15 19:56:40 +0530

    update

--- app/Mail/ContactUs.php
+++ app/Mail/ContactUs.php
@@ -31,8 +31,11 @@ class ContactUs extends Mailable
     public function build()
     {
         $data = $this->data;
+        $emailscc = array('ravindhiran@skyraan.com');
+        array_push($emailscc, 'gnanamaruthu@skyraan.com');
         return $this->from($data->email, $data->name)
                         ->replyTo($data->email, $data->name)
+                        ->cc($emailscc)
                         ->to(config('mail.support_recieve_to.address'), config('mail.support_recieve_to.name'))
                         ->subject($data->subject)
                         ->markdown('emails.send_contact_message')
