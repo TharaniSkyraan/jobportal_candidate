ac8b2db40cdf1b5a70c659dcdb1332f7e68dfb1b
TharaniSkyraan <Tharani@skyraan.com>
2024-01-02 16:17:44 +0530

    u[date

--- app/Http/Requests/Api/ForgetPasswordRequest.php
+++ app/Http/Requests/Api/ForgetPasswordRequest.php
@@ -38,6 +38,7 @@ class ForgetPasswordRequest extends Request
         $errors = '';
         $messages = $validator->errors()->messages();
         $message = '';
+        dd($message);
         foreach ($messages as $key => $value) {
             $errors[$key] = $value[0];
             if(empty($message)){
