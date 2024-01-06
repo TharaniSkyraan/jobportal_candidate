87d28366cce9dae5f299f0c704b56939ed7b65ce
TharaniSKyraan <tharani@skyraan.com>
2024-01-06 15:38:39 +0530

    update

--- app/Http/Requests/Api/User/UpdateProfileRequest.php
+++ app/Http/Requests/Api/User/UpdateProfileRequest.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Request;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
 
 class UpdateProfileRequest extends Request
 {
@@ -32,7 +33,7 @@ class UpdateProfileRequest extends Request
         $email = Auth::user()->email;
         $rules =  [
             'name' => 'required|max:80',
-            'phone' => 'required|unique:users,phone,'.(Auth::user()->id).',id,deleted_at,NULL',
+            // 'phone' => 'required|unique:users,phone,'.(Auth::user()->id).',id,deleted_at,NULL',
             // 'skill_id' => 'required|unique:user_skills,skill_id,'.($this->id??null).',id,deleted_at,NULL,user_id,'.\Auth::user()->id,
             'image' => 'required',
             // 'alternative_phone' => 'required',
@@ -42,7 +43,24 @@ class UpdateProfileRequest extends Request
             'country_id' => 'required',
             'date_of_birth' => 'required',
         ];
-
+        $rules['phone'] = [
+            'required',
+            Rule::unique('users')->where(function ($query) {
+                return $query->where('phone', request('phone'))
+                             ->orWhere('alternative_phone', request('phone'))
+                             ->whereNull('deleted_at')
+                             ->where('id', '<>', \Auth::user()->id);
+            }),
+        ];
+        $rules['alternative_phone'] = [
+            'nullable',
+            Rule::unique('users')->where(function ($query) {
+                return $query->where('alternative_phone', request('alternative_phone'))
+                             ->orWhere('phone', request('alternative_phone'))
+                             ->whereNull('deleted_at')
+                             ->where('id', '<>', \Auth::user()->id);
+            }),
+        ];
         return $rules;
     }
     public function failedValidation(Validator $validator)
