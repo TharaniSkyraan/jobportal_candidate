9b8e9b47109fee242cf14a3cbd0d259cb444e7c0
TharaniSKyraan <tharani@skyraan.com>
2024-01-08 13:17:54 +0530

    update

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -55,7 +55,7 @@ class UserController extends BaseController
             'genders' => DataArrayHelper::langGendersApiArray(),    
             'maritalStatuses' => DataArrayHelper::langMaritalStatusesApiArray()
         );
-        return $this->sendResponse($response);
+        return $this->sendResponse([$response]);
         
     }
     
